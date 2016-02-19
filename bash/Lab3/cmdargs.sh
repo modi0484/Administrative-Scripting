5989d178dc5ea51ef7ba945fb2ac48e6f7d474be
modi0484 <modi0484@gmail.com>
2016-02-19 21:14:08 +0000

    Lab3-cmdargs.sh updated

--- bash/Lab3/cmdargs.sh
+++ bash/Lab3/cmdargs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-#
-# this script demonstrates using positional parameters
-# also known as command line options and arguments
 
+# loop through the command line arguments
 while [ $# -gt 0 ]; do
+    
+    # display each argument on its own line
 	echo "$1"
+	
+	# each time through the loop, shift the arguments left
+	# this decrements the argument count for us
 	shift
 done
